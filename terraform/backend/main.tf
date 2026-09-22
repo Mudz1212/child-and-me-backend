b18e29f2097f2c9d28f0e801181a890f65c8fbcf
Mudathir <mudathir3618@gmail.com>
2026-09-22 10:05:31 +0100

    Add terraform infrastructure config for child-and-me

--- terraform/backend/main.tf
+++ terraform/backend/main.tf
@@ -1,12 +1,22 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
+resource "azurerm_resource_group" "backend_rg" {
+  name     = "rg-tfstate-child-and-me"
+  location = "swedencentral"
+}
+
+resource "azurerm_storage_account" "backend_state" {
+  name                     = "stchildandmema"
+  resource_group_name      = azurerm_resource_group.backend_rg.name
+  location                 = azurerm_resource_group.backend_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  blob_properties {
+    versioning_enabled = true
   }
 }
 
-provider "azurerm" {
-  features {}
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.backend_state.name
+  container_access_type = "private"
 }
