3af28f68ac0f2168de027a49377746dd383388a6
Yonathan <yonathan.surafel62@gmail.com>
2026-09-24 12:06:44 +0100

    Added endpoints for POSTing amenities to db

--- server/controllers/amenities.js
+++ server/controllers/amenities.js
@@ -4,4 +4,24 @@ async function index(req, res) {
   res.json(await Amenity.findAll());
 }
 
-module.exports = { index };
+async function create(req, res) {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(400).json({
+        error: "Amenity name is required"
+      });
+    }
+    const amenity = await Amenity.create(name);
+    res.status(201).json(amenity);
+    
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: "Failed to create amenity"
+    });
+  }
+}
+
+module.exports = { index, create };
--- server/routers/amenities.js
+++ server/routers/amenities.js
@@ -2,4 +2,5 @@ const express = require("express");
 const controller = require("../controllers/amenities");
 const router = express.Router();
 router.get("/", controller.index);
+router.post("/", controller.create);
 module.exports = router;
