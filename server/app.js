3e5f872c16b695dc0acc76391656051686bf4f6d
Yonathan <yonathan.surafel62@gmail.com>
2026-09-24 19:01:57 +0100

    Endpoint for venue amenities changed

--- server/app.js
+++ server/app.js
@@ -75,7 +75,7 @@ app.use("/auth", authRouter);
 app.use("/venues", venuesRouter);
 app.use("/venues/:venueId/reviews", reviewsRouter);
 app.use("/amenities", amenitiesRouter);
-app.use("/venue-amenities", venueAmenitiesRouter);
+app.use("/venueAmenities", venueAmenitiesRouter);
 app.use("/geoapify", geoapifyRouter);
 
 module.exports = app;
