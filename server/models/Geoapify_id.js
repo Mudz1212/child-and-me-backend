27fa12d50a5960470492fc711c263d0d861543cd
Yonathan <yonathan.surafel62@gmail.com>
2026-09-25 15:56:19 +0100

    Added to Geoapify.js so the findByPlaceId function returns amenities along with the venue

--- server/models/Geoapify_id.js
+++ server/models/Geoapify_id.js
@@ -3,9 +3,33 @@ const db = require("../db/connect");
 class Geoapify {
   static async findByPlaceId(geoapifyPlaceId) {
     const result = await db.query(
-      "SELECT * FROM venues WHERE geoapify_place_id = $1",
-      [geoapifyPlaceId],
+      `
+      SELECT
+        v.*,
+        COALESCE(
+          json_agg(
+            json_build_object(
+              'id', a.id,
+              'name', a.name
+            )
+          ) FILTER (WHERE a.id IS NOT NULL),
+          '[]'
+        ) AS amenities
+      FROM venues v
+
+      LEFT JOIN venue_amenities va
+        ON v.id = va.venue_id
+
+      LEFT JOIN amenities a
+        ON va.amenity_id = a.id
+
+      WHERE v.geoapify_place_id = $1
+
+      GROUP BY v.id
+      `,
+      [geoapifyPlaceId]
     );
+
     return result.rows[0];
   }
 }
