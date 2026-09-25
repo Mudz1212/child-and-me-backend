bd72b6643ed4ad703a0470b2b033f8227c3755ee
Mudathir <mudathir3618@gmail.com>
2026-09-25 10:55:55 +0100

    Fix: Tests fixed

--- server/routers/venues.js
+++ server/routers/venues.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const controller = require("../controllers/venues");
-const { requireAuth, requireRole } = require("../middleware/auth");
+const { requireAuth } = require("../middleware/auth");
 const router = express.Router();
+
 router.get("/", controller.index);
-router.post(
-  "/import",
-  controller.importVenues,
-);
+router.post("/import", controller.importVenues);
 router.get("/:id", controller.show);
-router.post("/", controller.create);
+router.post("/", requireAuth, controller.create);
 router.put("/:id", requireAuth, controller.update);
 router.patch("/:id", requireAuth, controller.patch);
+
 module.exports = router;
